abf976aa513186a0b38d43e7e71f77e84d47084b
mohams11 <smohammadi1570@gmail.com>
2017-12-14 13:40:31 +0000

    method moved

--- src/DataStructure/Implementer.java
+++ src/DataStructure/Implementer.java
@@ -11,76 +11,15 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Stack;
 
-
 public class Implementer implements Controller {
 
-	Retrieve r;
-
-
-	HashMap<String, ArrayList<String>> stations;
-	HashMap<String, ArrayList<String>> linkedStations;
-
+	private Retrieve r;
+	private HashMap<String, ArrayList<String>> stations;
 
 	public Implementer() {
 		r = new Retrieve();
 		stations = new HashMap<>();
-		linkedStations = new HashMap<>();
 	}
-	
-	public void listStationLinks() 
-	{
-		//already have a hashmap of line with stations in a line
-		//to find station links you need to get a station and get the previous and next,
-		//considering that there is a previous and next and store that into an empty arraylist (links)
-		//then go through the next line and if that station is there do the previous next and add to links
-		//if there isn't go to the next line
-	
-		stations = r.getHashMap();
-		//assign new hashmap to the hashmap in Retrieve class (line is key, stations are value)
-		for (Entry<String, ArrayList<String>> entry : stations.entrySet()) 
-			//use entrySet to get a Set view of the mappings contained in the stations hashmap
-			//for every key value pair in the stations hashmap...
-		{
-			ArrayList<String> stationsInLine = entry.getValue();
-			//get the arraylist of stations value and put it into a new arraylist stationsInLine 
-			
-			for(int i = 0; i < stationsInLine.size(); i++) 
-				//go through the arraylist stationsInLine containing the stations from the value of the hashmap
-			{
-				String currentStation = stationsInLine.get(i);
-				//get current station at position i 
-				
-				ArrayList<String> links;
-				//create empty arraylist links for storing relevant stations 
-				
-				if(linkedStations.containsKey(currentStation)) 
-				//if another line contains the current station
-				//lamisha: if the linkedStations hashmap has the current station as the key
-				{
-					links = linkedStations.get(currentStation);
-					//get their links list and add the new link from the other line that links to this station
-					//lamisha: assign the current station to the links array list
-				} else 
-				{
-					links = new ArrayList<>();
-					//if it is the very first link then create create an arraylist
-				}
-				if(i!=0)
-				{
-					links.add(stationsInLine.get(i-1));
-					//if it is not the first item add the station previous to the one you are at - this will form a line of stations
-				}
-				if(i + 1 != stationsInLine.size()) 
-				{
-					links.add(stationsInLine.get(i + 1));
-					//if it is not the last item, add the next station to the current one
-				}
-				//populate the hashmap with station as key and the relevant links arraylist created
-				linkedStations.put(currentStation, links);
-			}
-			
-		}
-	} 
 
 	public String listAllTermini() {
 
@@ -93,12 +32,10 @@ public class Implementer implements Controller {
 
 		return terminiToString;
 	}
-	
 
 	public String listStationsInLine(String line) {
 
-		
-		stations = r.getHashMap();
+		stations = r.getStations();
 		String stationsToString = "";
 
 		for (String s : stations.get(line)) {
@@ -107,17 +44,16 @@ public class Implementer implements Controller {
 
 		return stationsToString;
 	}
-	
 
 	public String listAllDirectlyConnectedLines(String line) {
 		String connections = "";
 		try {
-			stations = r.getHashMap();
+			stations = r.getStations();
 			ArrayList<String> linkingLines = new ArrayList<>();
 			boolean isLinked;
 			connections = "The following lines are connected: ";
 			linkingLines = stations.get(line);
-			
+
 			for (String key : stations.keySet()) {
 
 				isLinked = !Collections.disjoint(linkingLines, stations.get(key));
@@ -134,63 +70,60 @@ public class Implementer implements Controller {
 
 	public String showPathBetween(String stationA, String stationB) {
 		try {
-		listStationLinks();
-		//store the stations we want to search in a stack 
-		Stack<String> stationsToBeSearched = new Stack<>();
-		//store visited nodes which do not lead to the destination in a HashSet
-		Set<String> irrelevantStations = new HashSet<>();
-		//use hashmap to store the destination station as String key and
-		//the path to reach that destination station as an ArrayList of String which is the value
-		HashMap<String, ArrayList<String>> pathToDestination = new HashMap<>();
-		
-		if(linkedStations.containsKey(stationA) && linkedStations.containsKey(stationB))
-		//whichever start station (from the stations hashmap) the user enters, put that into the destinationPath hashmap,
-		//making stationA (the start station) a key in the hashmap with no path as of yet
-		{
-			pathToDestination.put(stationA, new ArrayList<String>());
-			//add the user's first station into the Stack so we can search its links, being the obvious first station in the path
-			stationsToBeSearched.add(stationA);
-			while (stationsToBeSearched.isEmpty()==false)
-			//whilst the stack stationsToBeSearched  has stations in it
+			HashMap<String, ArrayList<String>> linkedStations = new HashMap<>();
+			linkedStations = r.getLinkedStations();
+			// store the stations we want to search in a stack
+			Stack<String> stationsToBeSearched = new Stack<>();
+			// store visited nodes which do not lead to the destination in a HashSet
+			Set<String> irrelevantStations = new HashSet<>();
+			// use hashmap to store the destination station as String key and
+			// the path to reach that destination station as an ArrayList of String which is
+			// the value
+			HashMap<String, ArrayList<String>> pathToDestination = new HashMap<>();
+
+			if (linkedStations.containsKey(stationA) && linkedStations.containsKey(stationB))
+			// whichever start station (from the stations hashmap) the user enters, put that
+			// into the destinationPath hashmap,
+			// making stationA (the start station) a key in the hashmap with no path as of
+			// yet
 			{
-				String currentBeingSearched = stationsToBeSearched.pop();
-				if(currentBeingSearched.equals(stationB)==false) 
+				pathToDestination.put(stationA, new ArrayList<String>());
+				// add the user's first station into the Stack so we can search its links, being
+				// the obvious first station in the path
+				stationsToBeSearched.add(stationA);
+				while (stationsToBeSearched.isEmpty() == false)
+				// whilst the stack stationsToBeSearched has stations in it
 				{
-					for(String s : linkedStations.get(currentBeingSearched))
-					{
-						if(irrelevantStations.contains(s)) 
-						{
-							continue;
+					String currentBeingSearched = stationsToBeSearched.pop();
+					if (currentBeingSearched.equals(stationB) == false) {
+						for (String s : linkedStations.get(currentBeingSearched)) {
+							if (irrelevantStations.contains(s)) {
+								continue;
+							}
+							if (currentBeingSearched.contains(s) == false) {
+								ArrayList<String> pathToNext = new ArrayList<>(
+										pathToDestination.get(currentBeingSearched));
+								pathToNext.add(currentBeingSearched);
+								pathToDestination.put(s, pathToNext);
+								stationsToBeSearched.add(s);
+							}
 						}
-						if(currentBeingSearched.contains(s)==false)
-						{
-							ArrayList<String> pathToNext = new ArrayList<>(pathToDestination.get(currentBeingSearched));
-							pathToNext.add(currentBeingSearched);
-							pathToDestination.put(s, pathToNext);
-							stationsToBeSearched.add(s);
-						}
+					} else {
+						stationsToBeSearched.clear();
+
 					}
+					irrelevantStations.add(currentBeingSearched);
 				}
-				else 
-				{
-					stationsToBeSearched.clear();
-					
-				}
-				irrelevantStations.add(currentBeingSearched);
+				ArrayList<String> finalPath = pathToDestination.get(stationB);
+				finalPath.add(stationB);
+				return finalPath.toString();
+			} else {
+				throw new Exception("station entered does not exist");
 			}
-			ArrayList<String> finalPath = pathToDestination.get(stationB);
-			finalPath.add(stationB);
-			return finalPath.toString();
-		} else 
-		{
-			throw new Exception("station entered does not exist");
+
+		} catch (Exception e) {
+			return e.getMessage();
 		}
-		
-	}
-	catch(Exception e) 
-	{
-		return e.getMessage();
-	}
 
 	}
 }
--- src/DataStructure/Retrieve.java
+++ src/DataStructure/Retrieve.java
@@ -4,74 +4,137 @@ import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.File;
 
-
 public class Retrieve {
 
 	private ArrayList<String> allTermini;
 
 	private String[] cells;
 
-	HashMap<String, ArrayList<String>> details;
+	private HashMap<String, ArrayList<String>> stations;
+	private HashMap<String, ArrayList<String>> linkedStations;
 
-	
 	public Retrieve() {
 		allTermini = new ArrayList<>();
 		cells = new String[20];
-		details = new HashMap<>();
+		stations = new HashMap<>();
+		linkedStations = new HashMap<>();
 		readFile();
+		createLinkedStructure();
 	}
-	
+
 	public void readFile() {
 		ArrayList<String> tempStations = new ArrayList<>();
-		
+
 		String mtrSystemFile = "MTRsystem_partial.csv";
 		File file = new File(mtrSystemFile);
-		
+
 		try {
 			Scanner inputStream = new Scanner(file);
-			
-			while(inputStream.hasNext()) 
-			{
+
+			while (inputStream.hasNext()) {
 				String line = inputStream.nextLine();
 				cells = line.split(",");
 				int celli = 1;
-				
-				
-				while(celli <= cells.length - 1)
-				{
-					if(celli == 1 || celli == cells.length - 1) {
+
+				while (celli <= cells.length - 1) {
+					if (celli == 1 || celli == cells.length - 1) {
 						allTermini.add((cells[celli]));
 					}
-					
+
 					tempStations.add(cells[celli]);
 					celli++;
 				}
-				
-				addToHashMap(cells[0] ,tempStations);
-				tempStations.clear();
-		
-				
-				
-				cells=null;
-			}
-			 inputStream.close();
-		    } catch (FileNotFoundException e) { 
-		      System.out.println("file not found");
-		    }
-	}
-	
-	public void addToHashMap(String lineName, ArrayList<String> s) {
-		
-		details.put(lineName, new ArrayList<String>(s));
-	}
-	
 
-	public HashMap<String, ArrayList<String>> getHashMap(){
-		return details;
+				addToStations(cells[0], tempStations);
+				tempStations.clear();
+
+				cells = null;
+			}
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found");
+		}
+	}
+
+	public void createLinkedStructure() {
+		// already have a hashmap of line with stations in a line
+		// to find station links you need to get a station and get the previous and
+		// next,
+		// considering that there is a previous and next and store that into an empty
+		// arraylist (links)
+		// then go through the next line and if that station is there do the previous
+		// next and add to links
+		// if there isn't go to the next line
+
+		// assign new hashmap to the hashmap in Retrieve class (line is key, stations
+		// are value)
+		for (ArrayList<String> s : stations.values())
+		// use entrySet to get a Set view of the mappings contained in the stations
+		// hashmap
+		// for every key value pair in the stations hashmap...
+		{
+			ArrayList<String> stationsInLine = s;
+			// get the arraylist of stations value and put it into a new arraylist
+			// stationsInLine
+			int i = 0;
+			while(i < stationsInLine.size())
+			// go through the arraylist stationsInLine containing the stations from the
+			// value of the hashmap
+			{
+				String currentStation = stationsInLine.get(i);
+				// get current station at position i
+
+				ArrayList<String> links;
+				// create empty arraylist links for storing relevant stations
+
+				if (linkedStations.containsKey(currentStation))
+				// if another line contains the current station
+				// lamisha: if the linkedStations hashmap has the current station as the key
+				{
+					links = linkedStations.get(currentStation);
+					// get their links list and add the new link from the other line that links to
+					// this station
+					// lamisha: assign the current station to the links array list
+				} else {
+					links = new ArrayList<>();
+					// if it is the very first link then create create an arraylist
+				}
+				
+				if (i + 1 < stationsInLine.size()) {
+					links.add(stationsInLine.get(i + 1));
+					// if it is not the last item, add the next station to the current one
+				}
+				
+				if (i > 0) {
+					links.add(stationsInLine.get(i - 1));
+					// if it is not the first item add the station previous to the one you are at -
+					// this will form a line of stations
+				}
+				
+				// populate the hashmap with station as key and the relevant links arraylist
+				// created
+				linkedStations.put(currentStation, links);
+				i++;
+			}
+
+		}
+	}
+
+	public void addToStations(String lineName, ArrayList<String> s) {
+
+		stations.put(lineName, new ArrayList<String>(s));
+	}
+
+	public HashMap<String, ArrayList<String>> getStations() {
+		return stations;
 	}
 	
-	public ArrayList<String> getAllTermini(){
+	public HashMap<String, ArrayList<String>> getLinkedStations(){
+		return linkedStations;
+	}
+
+	public ArrayList<String> getAllTermini() {
 		return allTermini;
 	}
-	
+
 }
